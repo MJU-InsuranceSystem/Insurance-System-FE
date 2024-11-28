a82c86ea3e4774145a56490049adc25b97b85a1a
sehyun0329 <tpgus4857@naver.com>
2024-11-28 10:39:44 +0900

    백엔드 api 주소 수정 (https)

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     open: true, // 서버 시작 시 브라우저 열기
     proxy: {
       '/api': {
-        target: 'http://localhost:8080', // 백엔드 서버 주소
+        target: 'https://api.mju-insurance.xyz', // 백엔드 서버 주소
         changeOrigin: true, // Origin 헤더 변경
         rewrite: (path) => path.replace(/^\/api/, '') // 경로 재작성
       }
